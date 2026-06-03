b6dabed4afde77ef68c60e3d19e97cf200e2a040
Mahtab Mahi <mahtab.mahi707@gmail.com>
2026-06-03 10:40:56 +0600

    Time: 63 ms (19.96%) | Memory: 92.9 MB (12.80%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       
+       unordered_map<int,int> umap;
+
+       for(int i = 0 ; i< nums.size();i++){
+        umap[nums[i]]++;
+       }
+       for( int i = 0 ; i<nums.size(); i++){
+        if(umap[nums[i]]>1) return true;
+       }
+       return false;
+
+        
+
+        
+    }
+};
