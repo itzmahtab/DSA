8e72e04fd8655e557de2ad92772340a9196f3bf3
Mahtab Mahi <mahtab.mahi707@gmail.com>
2026-06-03 10:55:14 +0600

    Time: 0 ms (100.00%) | Memory: 10 MB (10.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> ma;
-                unordered_map<char,int> mb;
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        if(s.size()!= t.size()) return false;
 
-                for(auto &el:s){
-                    ma[el]++;
-                }
-                for(auto &el:t){
-                    mb[el]++;
-                }
-                return ma==mb;
+    for ( int i = 0 ; i< s.size(); i++){
+        m1[s[i]]++;
+        m2[t[i]]++;
+    }
+    if(m1==m2) return true;
+    return false;
 
-       
+        
     }
 };
